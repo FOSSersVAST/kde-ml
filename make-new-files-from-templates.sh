3f67e3a16327a5f22b4e21a8a9fac0feec8f93d0
Subin Siby <mail@subinsb.com>
2019-11-28 16:51:25 +0530

    * Make script for generating new files from templates * Sync with KDE Upstream * Remove unwanted

---
+++ make-new-files-from-templates.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Make new .po files
+pots=$(find l10n-kf5/templates/ -name *.pot -type f)
+for templatepath in $pots; do
+    filepath=${templatepath/templates\//ml/}
+    filepath=${filepath/.pot/.po}
+    if [ ! -f $filepath ]; then
+        msginit -o $filepath -i $templatepath --no-translator --locale=ml_IN
+    fi
+    sed -i 's/Language-Team: none/Language-Team: SMC <smc.org.in>/' $filepath
+done
