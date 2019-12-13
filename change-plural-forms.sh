49e42b4306f24c0c2dd74dede4d7dfca4d2bc325
Subin Siby <mail@subinsb.com>
2019-12-13 23:00:50 +0530

    Fix plurals

--- change-plural-forms.sh
+++ change-plural-forms.sh
@@ -1,10 +1,10 @@
  #!/bin/bash
 
 # Fix Plural Forms value in .po files
-pos=$(find l10n-kf5/ml/kdegames -name *.po -type f)
+pos=$(find l10n-kf5/ml -name *.po -type f)
 for path in $pos; do
     if [ -f $path ]; then
         echo $path
-        sed -i "s/Plural-Forms: nplurals=INTEGER; plural=EXPRESSION;/Plural-Forms: nplurals=2; plural=(n != 1);/" $path
+        sed -i "s/Plural-Forms: .*;/Plural-Forms: nplurals=2; plural=(n != 1);/" $path
     fi
 done
